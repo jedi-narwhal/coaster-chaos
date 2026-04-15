972e28e9b54e610b7e8243929a8886c13aea3c11
Justin Li <jdli3@uci.edu>
2026-04-15 00:19:18 -0700

    1-minute prototype

--- Scripts/player_camera.gd
+++ Scripts/player_camera.gd
@@ -3,6 +3,6 @@ extends Camera2D
 @onready var player: CharacterBody2D = $"../Player"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var player_x = player.global_position.x
 	global_position = Vector2(player_x, global_position.y)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,51 @@
 extends CharacterBody2D
 
-const SPEED := 30.0
-const SWITCH_DURATION = 0.15
+const SPEED := 50.0
+const SWITCH_DURATION := 0.2
+
+# Change this if a new track's height is taller
+const MAX_TRACK_HEIGHT := 20
+
+# Change this to change how far up/down the cart can see
+var switch_track_dist := 100
+
+var forward_direction := Vector2.RIGHT
 
 @onready var up_raycast: RayCast2D = $UpRayCast
 @onready var down_raycast: RayCast2D = $DownRayCast
-@onready var track_layer: TileMapLayer = $"../TrackLayer"
 
 var _switching_track := false
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("rolling")
-	velocity.x = SPEED
+	up_raycast.target_position.y = -switch_track_dist
+	down_raycast.target_position.y = switch_track_dist
+	velocity = SPEED * forward_direction
 
 func _physics_process(delta: float) -> void:
 	# Ignore physics if currently tweening
 	if _switching_track:
-		position.x += SPEED * delta
-		velocity.y = 0.0
 		return
 	
 	# Apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		velocity = SPEED * forward_direction
 	
+	_set_forward_direction()
+	_rotate_children()
 	_handle_jumps()
 	move_and_slide()
+
+func _rotate_children() -> void:
+	if _get_floor_normal() == Vector2.INF:
+		return
+	var rotation_angle: float = _get_floor_normal().angle() + PI / 2
+	$AnimatedSprite2D.global_rotation = rotation_angle
+	$CollisionShape2D.global_rotation = rotation_angle
+	up_raycast.global_rotation = rotation_angle
+	down_raycast.global_rotation = rotation_angle
 
 func _handle_jumps() -> void:
 	var target_pos: Vector2
@@ -33,27 +53,58 @@ func _handle_jumps() -> void:
 	# Jump to higher track
 	if Input.is_action_just_pressed("up") and \
 	is_on_floor() and up_raycast.is_colliding():
-		target_pos = _get_track_position(
-			up_raycast.get_collision_point())
-		_switch_to_track(target_pos.y)
+		target_pos = _get_track_position(up_raycast)
+		if target_pos != Vector2.INF:
+			_switch_to_track(target_pos)
 	
 	# Drop to lower track
 	if Input.is_action_just_pressed("down") and \
 	is_on_floor() and down_raycast.is_colliding():
-		target_pos = _get_track_position(
-				down_raycast.get_collision_point())
-		_switch_to_track(target_pos.y)
+		target_pos = _get_track_position(down_raycast)
+		if target_pos != Vector2.INF:
+			_switch_to_track(target_pos)
 
-func _get_track_position(global_hit: Vector2) -> Vector2:
-	# Get track position based on the cell
-	var local_hit: Vector2 = track_layer.to_local(global_hit)
-	var cell: Vector2 = track_layer.local_to_map(local_hit)
-	var tile_pos: Vector2 = track_layer.map_to_local(cell)
-	var global_pos: Vector2 = track_layer.to_global(tile_pos)
+func _get_track_position(raycast: RayCast2D) -> Vector2:
+	var collision_point: Vector2 = raycast.get_collision_point()
+	var collision_normal: Vector2 = raycast.get_collision_normal()
 	var half_cart_height: float = $CollisionShape2D.shape.extents.y
-	return global_pos - Vector2(0, half_cart_height + 0.8)
+	
+	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+	var forward: Vector2 = SPEED * forward_direction * SWITCH_DURATION
+	
+	# hit underneath a track (normal is pointing downwards if positive y)
+	# create a new raycast above the track to find the top of the rail
+	if collision_normal.y > 0:
+		var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
+			collision_point - MAX_TRACK_HEIGHT * collision_normal,
+			collision_point
+		)
+		query.exclude = [self]
+		var result: Dictionary = space_state.intersect_ray(query)
+		if result:
+			return forward + result["position"] + result["normal"] * half_cart_height
+	else:
+		var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
+			collision_point - MAX_TRACK_HEIGHT * collision_normal,
+			collision_point - switch_track_dist * collision_normal,
+		)
+		query.exclude = [self]
+		var result: Dictionary = space_state.intersect_ray(query)
+		if result:
+			return forward + result["position"] + result["normal"] * half_cart_height
+	return Vector2.INF
 
-func _switch_to_track(target_y) -> void:
+func _get_floor_normal() -> Vector2:
+	if down_raycast.is_colliding():
+		return down_raycast.get_collision_normal()
+	return Vector2.INF
+
+func _set_forward_direction() -> void:
+	var floor_normal: Vector2 = _get_floor_normal()
+	if floor_normal != Vector2.INF:
+		forward_direction = Vector2(-floor_normal.y, floor_normal.x)
+
+func _switch_to_track(target: Vector2) -> void:
 	_switching_track = true
 	
 	var tween = create_tween()
@@ -61,12 +112,13 @@ func _switch_to_track(target_y) -> void:
 	tween.set_trans(Tween.TRANS_QUINT)
 	tween.tween_property(
 		self,
-		"global_position:y",
-		target_y,
+		"global_position",
+		target,
 		SWITCH_DURATION
 	)
 	
 	# Return to normal physics on callback
 	tween.tween_callback(func():
 		_switching_track = false
+		velocity = SPEED * forward_direction
 	)
