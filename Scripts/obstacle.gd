72598f6a42a10f5ead6cf44c19debffba367250a
Davidps255 <davidps@uci.edu>
2026-04-22 15:04:56 -0700

    Connect obstacle collision
    Now affects speed and score

--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@export var score_manager: Control
+
+@export var speed_reduction := 50.0 / 100.0 # First number is the percentage change
+@export var score_decrement := 10
+
 func _on_body_entered(body: Node2D) -> void:
 	'''
 	Removes -1 health from body if it collides
@@ -11,6 +16,8 @@ func _on_body_entered(body: Node2D) -> void:
 	# if health > 1 : remove -1 health
 	if body.health() > 1:
 		body.remove_health()
+		body.speed -= body.speed * speed_reduction
+		score_manager.mod_score(-1 * score_decrement)
 	else: # else : jump to game over scene
 		# for now this prints game over
 		body.remove_health()
