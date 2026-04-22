bb3280e6ea86f3b6bbd5051a4f22b1075909da0c
Stephanie Lee <stephl25@uci.edu>
2026-04-22 13:36:20 -0700

    Added obstacle scene and script

---
+++ Scripts/obstacle.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	'''
+	Removes -1 health from body if it collides
+	with the obstacle.
+	
+	:param body: Should only be the player's Node2D
+	'''
+
+	print("popcorn yummy") #FIXME: replace with health logic
+
+	# if health > 1 : remove -1 health
+	if body.health() > 1:
+		body.remove_health()
+	else: # else : jump to game over scene
+		# for now this prints game over
+		print("Game Over")
+	
+	queue_free()
